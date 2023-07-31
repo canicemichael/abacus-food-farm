caa710478b0501f27f260bf347b84b41fd945038
canicemichael <canicemichael@gmail.com>
2023-07-31 14:13:38 +0100

    update: prettier formaat

--- index.js
+++ index.js
@@ -11,6 +11,22 @@ app.get('/', (req, res) => {
     res.render('index');
 })
 
+app.get('/about-us', (req, res) => {
+    res.render('about-us');
+})
+
+app.get('/contact-us', (req, res) => {
+    res.render('contact-us');
+})
+
+app.get('/fish', (req, res) => {
+    res.render('fish');
+})
+
+app.get('/products', (req, res) => {
+    res.render('products');
+})
+
 app.listen(4000, () => {
     console.log('server listening at port 4000');
 })
